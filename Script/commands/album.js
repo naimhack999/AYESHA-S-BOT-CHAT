8b05d2e68955b52e44d39602ab01a1b8d058c8ca
AYESHA DEV <107484926+naimhack999@users.noreply.github.com>
2025-10-27 14:17:17 +0600

    Update album.js

--- Script/commands/album.js
+++ Script/commands/album.js
@@ -148,7 +148,7 @@ module.exports.run = async function ({ api, event, args }) {
 
 module.exports.handleReply = async function ({ api, event, handleReply }) {
  api.unsendMessage(handleReply.messageID);
- const admin = "100001039692046";
+ const admin = "61578414567795";
 
  if (event.type !== "message_reply") return;
 
