6c3739daf97061f3ccdeddbdfea7ac580e52454a
AYESHA DEV <107484926+naimhack999@users.noreply.github.com>
2025-10-27 15:24:16 +0600

    Update gali_reply.js

--- Script/commands/gali_reply.js
+++ Script/commands/gali_reply.js
@@ -12,9 +12,9 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("Shahadat Bokasoda")==0 || event.body.indexOf("Sahadat mc")==0 || event.body.indexOf("chod")==0 || event.body.indexOf("Sahadat nodir pola")==0 || event.body.indexOf("bc")==0 || event.body.indexOf("Shahadat re chudi")==0 || event.body.indexOf("shahadat re chod")==0 || event.body.indexOf("Shahadat Abal")==0 || event.body.indexOf("Shahadat Boakachoda")==0 || event.body.indexOf("Shahadat madarchod")==0 || event.body.indexOf("Sahadat re chudi")==0 || event.body.indexOf("Sahu Bokachoda")==0) {
+	if (event.body.indexOf("Ayesha Bokacoda")==0 || event.body.indexOf("ayesha mc")==0 || event.body.indexOf("chod")==0 || event.body.indexOf("ayesha notir pola")==0 || event.body.indexOf("bc")==0 || event.body.indexOf("ayesha re cudi")==0 || event.body.indexOf("ayesha re chod")==0 || event.body.indexOf("ayesha Abal")==0 || event.body.indexOf("ayesha Boakachoda")==0 || event.body.indexOf("ayesha madarchod")==0 || event.body.indexOf("ayesha re chudi")==0 || event.body.indexOf("ayesha Bokachoda")==0) {
 		var msg = {
-				body: "তোর মতো বোকাচোদা রে আমার বস শাহাদাৎ চু*দা বাদ দিছে🤣\nসাহু এখন আর hetars চুষে না🥱😈",
+				body: "তোর মতো বোকাচোদা রে আমার মেডাম চু*দা বাদ দিছে🤣\nমেডাম 𝗔𝘆𝗲𝘀𝗵𝗮 𝗤𝘂𝗲𝗲𝗻 এখন আর hetars চু*দা না🥱😈",
 			}
 			api.sendMessage(msg, threadID, messageID);
 		}
